b4ded750ee872fa37f953fdce0d37769ef1171d1
Michael Harris <mharris021@hotmail.com>
2020-02-04 13:39:46 -0600

    added discovery service properties

--- cloudbees-todo-config-service/src/main/groovy/org/mharris/cloudbees/todo/configservice/TodoConfigServiceApplication.groovy
+++ cloudbees-todo-config-service/src/main/groovy/org/mharris/cloudbees/todo/configservice/TodoConfigServiceApplication.groovy
@@ -2,10 +2,12 @@ package org.mharris.cloudbees.todo.configservice
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 class TodoConfigServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(TodoConfigServiceApplication.class)
