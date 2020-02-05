c377b8becb0b9489414fb3a7f4cfe0ef47293f65
Michael Harris <mharris021@hotmail.com>
2020-02-04 20:57:12 -0600

    removed unneeded groovy dependencies

---
+++ cloudbees-todo-discovery-service/src/main/java/org/mharris/cloudbees/todo/discoveryservice/TodoDiscoveryServiceApplication.java
@@ -0,0 +1,14 @@
+package org.mharris.cloudbees.todo.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class TodoDiscoveryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TodoDiscoveryServiceApplication.class);
+    }
+
+}
--- cloudbees-todo-gateway-service/src/test/java/org/mharris/cloudbees/todo/gatewayservice/TodoGatewayServiceApplicationTest.java
+++ cloudbees-todo-gateway-service/src/test/java/org/mharris/cloudbees/todo/gatewayservice/TodoGatewayServiceApplicationTest.java
@@ -3,8 +3,6 @@ package org.mharris.cloudbees.todo.gatewayservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class TodoGatewayServiceApplicationTest {
 
