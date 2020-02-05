64d12f00ac20e7420caca5607559fb73f457218a
Michael Harris <mharris021@hotmail.com>
2020-02-04 22:02:46 -0600

    started todo-web-application

---
+++ cloudbees-todo-web-application/src/test/java/org/mharris/cloudbees/todo/webapplication/TodoWebApplicationTest.java
@@ -0,0 +1,14 @@
+package org.mharris.cloudbees.todo.webapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TodoWebApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
